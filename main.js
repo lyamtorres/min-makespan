5f174df09b7a33681020555962e32b3d56827c65
lyamtorres <lyamtorres@hotmail.com>
2021-11-19 11:12:06 +0100

    Add HTML basic user interface

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 class Mode {
-    constructor(_m) {
-        this.m = _m;
+    constructor(m) {
+        this.m = m;
     }
 
     describe() {
         console.log(this);
     }
 
-    produce() {
+    produceResult() {
         console.log(`
         Borne inférieure "maximum" =
         Borne inférieure "moyenne" =
 
-        Résultat LSA = ${this.LSA()}
+        Résultat LSA = ${this.LSA(false)}
         ratio LSA = 
 
         Résultat LPT = ${this.LPT()}
@@ -34,6 +34,7 @@ class Mode {
     LSA(useLPT) {
         let minorIndex;
         const machines = new Array(this.m).fill(0);
+        console.log(`machines: ${[...machines]}`);
         const tasks = [...this.D];
 
         if (useLPT) tasks.sort().reverse();
@@ -71,14 +72,14 @@ class Mode {
     }
 }
 
-class ImMode extends Mode {
-    constructor(_m) {
-        super(_m);
-        this.n = Math.pow(_m, 2) + 1; // Nombre de tâches (m^2 + 1)
-        this.D = this.getTasks(); // Tâches
+class Im extends Mode {
+    constructor(m) {
+        super(m);
+        this.n = Math.pow(m, 2) + 1; // Nombre de tâches (m^2 + 1)
+        this.D = this.generateTasksDuration(); // Tâches
     }
 
-    getTasks() {
+    generateTasksDuration() {
         const tasks = new Array(this.n-1).fill(1, 0, this.n-1);
         tasks.push(this.m);
 
@@ -86,14 +87,14 @@ class ImMode extends Mode {
     }
 }
 
-class IPrimeMMode extends Mode {
-    constructor(_m) {
-        super(_m);
-        this.n = 2 * _m + 1;
-        this.D = this.getTasks();
+class IPrimeM extends Mode {
+    constructor(m) {
+        super(m);
+        this.n = 2 * m + 1;
+        this.D = this.generateTasksDuration();
     }
 
-    getTasks() {
+    generateTasksDuration() {
         let i = 0;
         let count = this.m;
         const tasks = new Array(this.n);
@@ -110,30 +111,121 @@ class IPrimeMMode extends Mode {
     }
 }
 
-class IRMode {
-    constructor(_m, _n, _k, _dmin, _dmax) {
-        this.m = _m;
-        this.n = _n;
-        this.k = _k;
-        this.dmin = _dmin;
-        this.dmax = _dmax;
-        this.M = new Array(_m).fill(0);
-        this.D = this.getTasks();
-        this.I = new Array(_k);
+class IR extends Mode {
+    constructor(m, n, dmin, dmax) {
+        super(m);
+        this.n = n;
+        this.dmin = dmin;
+        this.dmax = dmax;
+        this.D = this.generateTasksDuration();
     }
 
-    getTasks() {
+    generateTasksDuration() {
         return Array.from({length: this.n}, () => Math.floor(Math.random() * (this.dmax - this.dmin + 1)) + this.dmin);
+    }
+
+    static getAverageRatio(arr) {
+        let lsaRatio = 0;
+        let lptRatio = 0;
+        let rmaRatio = 0;
+
+        for (let value of arr) {
+            lsaRatio += value.LSA(false);
+            lptRatio += value.LPT();
+            rmaRatio += value.RMA();
+        }
+
+        lsaRatio /= arr.length;
+        lptRatio /= arr.length;
+        rmaRatio /= arr.length;
+
+        console.log(`
+        ratio moyen LSA = ${lsaRatio}
+        ratio moyen LPT = ${lptRatio}
+        ratio moyen RMA = ${rmaRatio}
+        `);
     }
 }
 
-/* Instantiation d'objet */
+/* Genération d'une instance de type I'm */
 
-/* const inst = new ImMode(3);
-inst.produce(); */
+//const m = 3;
 
-const inst = new IPrimeMMode(3);
-inst.describe();
-inst.produce();
+//iPrimeM = new IPrimeM(m);
+//iPrimeM.produceResult();
 
+/* Genération aléatoire de plusieurs instances */
 
+/* const m = 3;
+const n = 9;
+const k = 3;
+const dmin = 1;
+const dmax = 3;
+
+const instances = [];
+
+for (let i = 0; i < k; i++) {
+    instances.push(new IR(m, n, dmin, dmax));
+}
+
+IR.getAverageRatio(instances); */
+
+/* Cache ou affiche les champs en fonction de l'algorithme choisi */
+document.querySelector("#algorithms").addEventListener("change", () => {
+    let selected = document.querySelector("#algorithms")
+              .options[document.querySelector("#algorithms").selectedIndex].value;
+
+    document.querySelector("#algorithms").value = selected;
+    if (selected === "rma") {
+        document.querySelector("#n").setAttribute("type", "text");
+        document.querySelector("#k").setAttribute("type", "text");
+        document.querySelector("#dmin").setAttribute("type", "text");
+        document.querySelector("#dmax").setAttribute("type", "text");
+    } else {
+        document.querySelector("#n").setAttribute("type", "hidden");
+        document.querySelector("#k").setAttribute("type", "hidden");
+        document.querySelector("#dmin").setAttribute("type", "hidden");
+        document.querySelector("#dmax").setAttribute("type", "hidden");
+    }
+});
+
+/* Fait parvenir les valeurs saisies par l'utilisateur */
+document.querySelector("#submit").onclick = () => {
+    let selected = document.querySelector("#algorithms")
+              .options[document.querySelector("#algorithms").selectedIndex].value;
+
+    switch (selected) {
+        case "lsa":
+            const m = document.querySelector("#m").valueAsNumber;
+            generateIm(m);
+            break;
+
+        case "lpt":
+            console.log("lpt");
+            break;
+
+        case "rma":
+            console.log("rma");
+            break;
+
+        default:
+            console.log("Choisissez un algorithme.");
+            break;
+    }
+}
+
+/* Genération d'une instance de type Im */
+
+const generateIm = (m) => {
+    const im = new Im(m);
+    im.describe();
+    im.produceResult();
+    //im.produceResult();
+}
+
+//const m = 3;
+
+//im = new Im(m);
+//im.produceResult();
+
+// TO-DO: Créer un switch pour changer l'instance en fonction du choix de l'utilisateur
